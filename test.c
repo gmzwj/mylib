bd9f62834e299bb9b89662bff251df56f79ef740
miaogao <miao.gao@ingenic.com>
2019-08-02 15:01:22 +0800

    第一次发布

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int add_gm(int a,int b);
+int sub_gm(int a,int b);
+int div_gm(int a,int b);
+
+int main(int argc,char *argv[])
+{
+	int a=10 , b=5;
+
+	printf("%d+%d=%d\n", a, b, add_gm(a,b));
+	printf("%d-%d=%d\n", a, b, sub_gm(a,b));
+	printf("%d/%d=%d\n", a, b, div_gm(a,b));
+
+	return 0;
+}
